072a2b52abeb2b02340e4c2441f22d086f8e7a2d
Hasan Balbaki <hsnblbki@gmail.com>
2022-08-27 16:54:10 +0200

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
+import '@laylazi/bootstrap-rtl/dist/js/bootstrap.min.js';
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/popper.min.js';
+import '@fortawesome/fontawesome-free/js/all.min.js';
+import '../sass/style.scss';
+
+
+$(window).scroll(function(){
+    if($(this).scrollTop() >= 100){
+        $("#navbar").addClass("noTransparent");
+    }
+    else{
+        $("#navbar").removeClass("noTransparent");
+    }
+});
+
+var date=new Date();
+var year=date.getFullYear();
+document.getElementById("date").innerHTML = year;
---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/',
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 9000,
+        writeToDisk: true,
+        open: true,
+    },
+
+    module: {
+        rules: [
+
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+
+            {
+                test: /\.(sass|css|scss)$/,
+                use: [
+
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                                publicPath: "../",
+                        },
+                    }, 
+
+                "css-loader",
+                
+                "sass-loader",
+                ],
+            },
+
+            {
+                test: /\.(jpg|png|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'images',
+                        },
+                    },
+                ],
+            },
+
+            
+            {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            use: [
+                    {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts',
+                        },
+                    },
+                ],
+            },
+
+            {
+                test: require.resolve("jquery"),
+                loader: "expose-loader",
+                options: {
+                    exposes: ["$", "jQuery"],
+                },
+            },
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template:"./src/index.html",
+            filename: "index.html"
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: "css/Style.css",
+        })
+    ],
+};
